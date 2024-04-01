45b0aed4377d5e979e5ef33052cfa847880cae99
ahai <1149956305@qq.com>
2024-04-01 15:03:44 +0800

    update ci

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "java"
 --- Plugin version
-PLUGIN.version = "0.1.0"
+PLUGIN.version = "0.1.1"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-java"
 --- Plugin license, please choose a correct license according to your needs.
