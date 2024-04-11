9853f82c062007722eb83ffeaff1e8c42eefd40f
lihan <lihan@apache.org>
2024-04-11 09:54:03 +0800

    fix openjdk path
    fix #7

--- hooks/post_install.lua
+++ hooks/post_install.lua
@@ -1,3 +1,5 @@
+local strings = require("vfox.strings")
+
 function PLUGIN:PostInstall(ctx)
     if RUNTIME.osType ~= "darwin" then
         return
@@ -5,12 +7,15 @@ function PLUGIN:PostInstall(ctx)
     local sdkInfo = ctx.sdkInfo['java']
     local path = sdkInfo.path
     local majorVersion = sdkInfo.note
+    local v = strings.split(sdkInfo.version, "+")[1]
     local needRemoveDir = {
+        '/jdk-' .. v .. '.jdk',
         '/jdk-' .. majorVersion .. '.jdk',
         '/Contents/Home'
     }
     print("Checking if need to rename jdk files...")
     for _, dir in ipairs(needRemoveDir) do
+        print("Checking path: " .. path .. dir)
         if checkDir(path .. dir) then
             print("Renaming jdk files: " .. path .. dir .. '/*')
             if os.execute('mv ' .. path .. dir .. '/*' .. ' ' .. path) == 1 then
