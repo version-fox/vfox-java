af6ee3280280bab4153a230930a7810e746a9c4b
lihan <lihan@apache.org>
2024-04-11 17:08:02 +0800

    fix: graalvm home
    fix #1

--- hooks/env_keys.lua
+++ hooks/env_keys.lua
@@ -1,10 +1,12 @@
+local strings = require("vfox.strings")
 --- Each SDK may have different environment variable configurations.
 --- This allows plugins to define custom environment variables (including PATH settings)
 --- Note: Be sure to distinguish between environment variable settings for different platforms!
 function PLUGIN:EnvKeys(ctx)
     local mainSdkInfo = ctx.main
     local path = mainSdkInfo.path
-    return {
+    local shortname = strings.split(mainSdkInfo.version, "-")[2]
+    local envTable = {
         {
             key = "JAVA_HOME",
             value = path
@@ -14,4 +16,11 @@ function PLUGIN:EnvKeys(ctx)
             value = path .. "/bin"
         }
     }
+    if shortname == "graal" or shortname == "graalce" then
+        table.insert(envTable, {
+            key = "GRAALVM_HOME",
+            value = path
+        })
+    end
+    return envTable
 end
