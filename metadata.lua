5c8987d075613ac402eae464e31c3aa1ce79815f
Han Li <lihan@apache.org>
2026-01-02 02:32:45 +0000

    release v0.5.1

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "java"
 --- Plugin version
-PLUGIN.version = "0.5.0"
+PLUGIN.version = "0.5."1
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-java"
 --- Plugin license, please choose a correct license according to your needs.
