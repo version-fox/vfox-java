46b401e8c407ce2ed884c34fd7777aab9070a3f9
Han Li <lihan@apache.org>
2026-01-02 02:23:21 +0000

    fix: support fuzzy match

--- hooks/pre_use.lua
+++ hooks/pre_use.lua
@@ -1,15 +1,24 @@
 local distribution_version_parser = require("distribution_version")
 
 function PLUGIN:PreUse(ctx)
-    local distribution_version = distribution_version_parser.parse_version(ctx.version)
+    local input_version = ctx.version
+    local distribution_version = distribution_version_parser.parse_version(input_version)
+    
+    -- If it is a pure numeric major version (such as "8"), and the distribution is openjdk, let the core perform fuzzy matching.
+    if distribution_version and distribution_version.distribution.short_name == "open" then
+        if not string.find(distribution_version.version, "%.") then  -- Pure numbers, no minor versions
+            return ""  -- Return empty, let vfox core perform fuzzy matching
+        end
+    end
+    
+
     if distribution_version and distribution_version.distribution.short_name ~= "open" then
         return {
             version = distribution_version.version .. "-" .. distribution_version.distribution.short_name
         }
     else
         return {
-            version = ctx.version,
+            version = input_version,
         }
     end
 end
-
